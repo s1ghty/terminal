cfc94641bc5abf5a6138f408ce5c847b629e6bd7
exstassy <erikjan1207@gmail.com>
2026-03-19 18:19:47 -0700

    added touch, changed the cmd call to case

--- terminal.pas
+++ terminal.pas
@@ -24,12 +24,94 @@ begin
     Result := GetCurrentDir;
 end;
 
-procedure ShowPrompt;
+procedure CmdPwd;
 begin
-  Write(GetUserName, ' ', GetCurrentFolderName, ' $ ');
+    writeln(GetCurrentDir);
 end;
 
+{ Needs to be finished!!!
+procedure CmdCd(const Path: string);
 begin
-    ShowPrompt;
-    writeln;
+    SetCurrentDir(Path);
+end;
+}
+
+{ Make Cat with Readln(filename, text) showmessage(text)
+procedure CmdCat;
+begin
+    while not eof(MyFile) do
+    begin
+        readln(MyFile, text);
+        ShowMessage(text)
+    end;
+end;
+}
+
+procedure CmdTouch(const FileName: string);
+var
+    MyFile: TextFile;
+
+begin
+    if FileExists(FileName) then
+    begin
+        writeln('File already exists: ', FileName);
+        exit;
+    end;
+    if FileName = '' then
+    begin
+        writeln('Invalid file name. Use a proper file name');
+        exit;
+    end;
+
+    AssignFile(MyFile, FileName);
+    Rewrite(MyFile);
+    CloseFile(MyFile);
+
+    writeln('File created: ', FileName);
+end;
+
+procedure CmdHelp;
+begin
+    writeln('Available commands: ');
+    writeln('clear      - clear the screen');
+    writeln('pwd        - show current directory');
+    writeln('touch FILE - create a file');
+    writeln('exit       - exit terminal');
+end;
+
+procedure ShowPrompt;
+begin
+  Write(GetUserName, '@', GetCurrentFolderName, ' $ ');
+end;
+var
+    cmd, arg, input: string;
+    SpaceFinder: integer;
+
+begin
+    ClrScr;
+    repeat
+        ShowPrompt;
+        readln(input);
+
+        { Separates the command and the argument }
+        input := Trim(input);
+        SpaceFinder := Pos(' ', input);
+        if SpaceFinder > 0 then
+        begin
+            cmd := Copy(input, 1, SpaceFinder - 1);
+            arg := Trim(Copy(input, SpaceFinder + 1, Length(input) - SpaceFinder));
+        end
+        else
+        begin
+            cmd := input;
+            arg := '';
+        end;
+
+        Case cmd of
+            'help': CmdHelp;
+            'clear': ClrScr;
+            'pwd': CmdPwd;
+            'touch': CmdTouch(arg);
+        end;
+    until cmd = 'exit';
 end.
